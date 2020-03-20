30919750ba447517433cdeb57da347b944b593a0
Matthieu <rei_eva@hotmail.com>
2020-03-20 16:54:53 +0100

    update for captcha

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
 {
+    /** @var string */
     private $secretKey;
 
     /**
@@ -27,19 +28,21 @@ final class HomeController extends AbstractController
 
     /**
      * @Route("/", name="home")
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
     public function home(Request $request): Response
     {
         $form = $this->createForm(ContactType::class)->handleRequest($request);
-//        $gcaptcha = new ReCaptcha($this->secretKey);
-//        $resp = $gcaptcha->verify($request->request->get('g-captcha-response'), $request->getClientIp());
-//
-//        if (!$resp->isSuccess()) {
-//            $errors = $resp->getErrorCodes();
-//        }
+        $gcaptcha = new ReCaptcha($this->secretKey);
+        $resp = $gcaptcha->verify($request->request->get('g-captcha-response'), $request->getClientIp());
 
-        if ($this->handler->handle($form)) {
-//            dd($form);
+        if ($this->handler->handle($form) && !$resp->isSuccess()) {
+            $errors = $resp->getErrorCodes();
+            $request->getSession()->getFlashBag()->add('error', 'Message non envoyé');
+        }
+
+        if ($this->handler->handle($form) && $resp->isSuccess()) {
+            $request->getSession()->getFlashBag()->add('success', 'Message envoyé');
         }
 
         return $this->render('home.html.twig', ['form' => $form->createView(), 'error' => $errors ?? null]);
